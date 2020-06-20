94f7345674f42102376b10d953819d93f3bdd051
ikReza <ireza.kaiser00@gmail.com>
2020-06-20 18:01:52 +0600

    third commit addToCart

--- server/data.js
+++ server/data.js
@@ -1,51 +1,69 @@
 const Data = [
   {
     _id: "0",
-    image: "./images/shirt-1.jpg",
+    image:
+      "https://www.aponzone.com/image/cache/data/shirt/full-sleeve-shirt-05-aponzone-600x540.jpg",
     name: "Casual Shirt",
     category: "Shirt",
     price: 10,
     reviews: 2,
+    rating: 4.5,
+    inStock: 6,
   },
   {
     _id: "1",
-    image: "./images/shirt-1.jpg",
+    image:
+      "https://www.aponzone.com/image/cache/data/shirt/full-sleeve-shirt-05-aponzone-600x540.jpg",
     name: "Shirt A",
     category: "Shirt",
     price: 10,
     reviews: 2,
+    rating: 4.5,
+    inStock: 6,
   },
   {
     _id: "2",
-    image: "./images/shirt-1.jpg",
+    image:
+      "https://www.aponzone.com/image/cache/data/shirt/full-sleeve-shirt-05-aponzone-600x540.jpg",
     name: "Shirt B",
     category: "Shirt",
     price: 10,
     reviews: 2,
+    rating: 4.5,
+    inStock: 6,
   },
   {
     _id: "3",
-    image: "./images/shirt-1.jpg",
+    image:
+      "https://www.aponzone.com/image/cache/data/shirt/full-sleeve-shirt-05-aponzone-600x540.jpg",
     name: "Shirt C",
     category: "Shirt",
     price: 10,
     reviews: 2,
+    rating: 4.5,
+    inStock: 6,
   },
   {
     _id: "4",
-    image: "./images/shirt-1.jpg",
+    image:
+      "https://www.aponzone.com/image/cache/data/shirt/full-sleeve-shirt-05-aponzone-600x540.jpg",
     name: "Shirt D",
     category: "Shirt",
     price: 10,
-    reviews: 2,
+    reviews: 8,
+    rating: 4.5,
+    inStock: 0,
   },
   {
     _id: "5",
-    image: "./images/shirt-1.jpg",
+    image:
+      "https://www.aponzone.com/image/cache/data/shirt/full-sleeve-shirt-05-aponzone-600x540.jpg",
     name: "Shirt E",
     category: "Shirt",
     price: 10,
     reviews: 2,
+    rating: 4.5,
+    inStock: 20,
   },
 ];
 
